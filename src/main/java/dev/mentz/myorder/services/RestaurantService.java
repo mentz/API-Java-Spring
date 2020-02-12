2109010f309c16db82809adf182122a92c76c8aa
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2020-02-12 20:40:33 -0300

    RestaurantService agora usa NotFoundException

--- src/main/java/dev/mentz/myorder/services/RestaurantService.java
+++ src/main/java/dev/mentz/myorder/services/RestaurantService.java
@@ -3,6 +3,7 @@ package dev.mentz.myorder.services;
 import dev.mentz.myorder.api.dtos.CreateRestaurantDto;
 import dev.mentz.myorder.api.dtos.RestaurantResponseDto;
 import dev.mentz.myorder.entities.Restaurant;
+import dev.mentz.myorder.exceptions.NotFoundException;
 import dev.mentz.myorder.repositories.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,11 @@ public class RestaurantService {
 //        Optional dá possibilidade de receber um objeto vazio e lidar
 //        com isso de forma graciosa (sem exceções).
         Optional<Restaurant> optional = restaurantRepository.findById(id);
+
+        if (!optional.isPresent()) {
+            throw new NotFoundException("Não encontrado restaurante com id: " + id);
+        }
+
         Restaurant restaurant = optional.get();
 //        optional.orElse(Restaurant objARetornarCasooptionalSejaVazio);
 
